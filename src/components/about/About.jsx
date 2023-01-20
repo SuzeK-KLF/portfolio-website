807c9e9a82e8fd603fec33d5fec82dbe1d3a850d
suzek-klf <suzeklf@gmail.com>
2023-01-19 20:41:33 -0500

    completed about frame

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -18,20 +18,20 @@ const About = () => {
         </div>
 
         <div className="about_content">
-          <div className="about_card">
-            <article>
+          <div className="about_cards">
+            <article className="about_card">
               <FaAward className="about_icon" />
               <h5>Experience</h5>
               <small>5+ Years Working</small>
             </article>
 
-            <article>
+            <article className="about_card">
               <FiUsers className="about_icon" />
               <h5>Clients</h5>
               <small>200+ Worldwide</small>
             </article>
 
-            <article>
+            <article className="about_card">
               <VscFolderLibrary className="about_icon" />
               <h5>Projects</h5>
               <small>20+ Completed</small>
